c6a1fefd3e8ec2b03146ee585c387fb0b15ae984
faszjoska <kalocsaroland.exe@gmail.com>
2026-02-04 08:50:45 +0100

    gyak

--- feladat_001.py
+++ feladat_001.py
@@ -14,6 +14,14 @@ def jelszo(ssh):
     jelszo = input("Kérek egy szép hosszú bonyolult jelszót: ")
     ssh.send_config_set(f"ena pass {jelszo}")
 
+def titkosit(ssh):
+    ssh.send_config_set(f"service password-encryption")
+
+def banner(ssh):
+    ssh.send_config_set(f"banner motd -Jogosulatlanul bejelentkezni TILOS!-")
+
+def rapid(ssh):
+    ssh.send_config_set(f"span mode rapid-pvst")
 
 
 
@@ -33,7 +41,7 @@ try:
         # 2. f
         print(kapcsolat.find_prompt())       
         
-        # 3. f
+        #3. f
         mentes(kapcsolat)
         
         valasz = kapcsolat.send_command("sh start")
@@ -48,7 +56,28 @@ try:
 
         print(kapcsolat.send_command("sh run | include enable"))
 
+        #5. f
+        titkosit(kapcsolat)
 
+        print(kapcsolat.send_command("sh run | include enable"))
+
+        #6. f
+        banner(kapcsolat)
+
+        print(kapcsolat.send_command("sh run | include banner"))
+        print(kapcsolat.send_command("sh run | include password"))
+
+        #7. f
+        rapid(kapcsolat)
+
+        print(kapcsolat.send_command("sh run | include spanning-tree"))
+
+        try:
+            with open("konfig-mentve.txt", "w", encoding="utf-8")as cel:
+                cel.write(kapcsolat.send_command("sh run"))
+
+        except IOError as ex:
+           print("Valami hiba van:", ex) 
 
 except Exception as ex:
     print("Valami hiba van:", ex)
