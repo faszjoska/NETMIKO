fdf0f03fe70f44c76700f1fae4892bf3f751722e
faszjoska <kalocsaroland.exe@gmail.com>
2026-02-27 09:42:44 +0100

    nem jo a router id

--- router.py
+++ router.py
@@ -4,7 +4,7 @@ from getpass import getpass
 
 login_adatok = {
     "device_type": "cisco_ios",
-    "host": "192.168.40.110",
+    "host": "172.162.110.13",
     "username": "rolivagyok",
     "password": "kiskacsa",
 }
@@ -13,17 +13,35 @@ login_adatok = {
 try:
     with ConnectHandler(**login_adatok) as kapcsolat:
 
-       print(kapcsolat.send_command("sh ip ospf neighbor"))
+        print(kapcsolat.send_command("sh ip ospf neighbor"))
+        """ 
+        ospf_az =  kapcsolat.send_command("sh run | include router ospf").split(" ")
+        #print(ospf_az[-1])
+        halozat_az = input("Adj meg egy hálózat címet amit szeretnél hirdetni:")
+        print(kapcsolat.send_config_set([f"router ospf {ospf_az[-1]}", f"network {halozat_az} area 0"] ))
 
-       ospf_az =  kapcsolat.send_command("sh run | include router ospf").split(" ")
-       #print(ospf_az[-1])
-       halozat_az = input("Adj meg egy hálózat címet amit szeretnél hirdetni:")
-       print(kapcsolat.send_config_set([f"router ospf {ospf_az[-1]}", f"network {halozat_az} area 0"] ))
+        savszelesseg = int(input("Adj meg egy referencia sávszélességet(100, 1000, 10000):"))
+        while savszelesseg != 100 and savszelesseg != 1000 and savszelesseg != 10000: 
+            savszelesseg = int(input("Adj meg egy referencia sávszélességet(100, 1000, 10000):"))
+        print(kapcsolat.send_config_set([f"router ospf {ospf_az[-1]}", f"auto-cost reference-bandwidth {savszelesseg}"]))
 
-       savszelesseg = int(input("Adj meg egy referencia sávszélességet(100, 1000, 10000):"))
-       while savszelesseg != 100 or 1000 or 10000: 
-           savszelesseg = int(input("Adj meg egy referencia sávszélességet(100, 1000, 10000):"))
-       kapcsolat.send_config_set[f"routert ospf {ospf_az[-1]}", f"auto-cost reference-bandwidth {savszelesseg}"]
+        """     
+        router_id = input("Kérek egy router azonositőt:")
+        joe = True
+        router_id = router_id.split(".")
+
+        while len(router_id) != 4:
+            router_id = input("Kérek egy router azonositót:")
+        for i in router_id:
+            if int(i) <= 255:
+                joe
+            elif i[-1] == ".":
+                joe = False
+            
+            if joe:
+                print(i)
+                
+        print(router_id)
 
        
        
